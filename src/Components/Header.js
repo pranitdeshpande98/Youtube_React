cfd0e14f4e471627ce4db9a1b7cee75a15f05e48
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2024-05-26 12:30:06 -0400

    Sidebar functionality added and header completed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='grid grid-flow-col p-5 m-2 shadow-lg'>
+      <div className='flex col-span-1 mt-1'>
+        <img className='h-14 w-12 object-cover' alt='menu' src='https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-462145.png?f=webp' />
+        <img className='h-14 w-18 mx-2 object-cover' alt='youtube' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-fsMXGc-ZXP37qszL-e9khRmE7PQV3QUafkj6JftMcA&s' />
+      </div>
+
+      <div className='col-span-10 text-center px-95 py-3 h-14'>
+      <input 
+      className='w-1/2 border border-gray-400 rounded-l-full p-2 placeholder-gray-500 placeholder-opacity-75 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500' 
+      placeholder='Search' 
+      type='text'
+    />
+        <button className='border border-gray-400 px-5 py-2 rounded-r-full'> 🔍 </button>
+      </div>
+
+      <div className='col-span-1'>
+        <img className='h-12 mt-1'  alt='user' src='https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png' />
+      </div>
+    </div>
   )
 }
 
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,7 +2,31 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className="p-5 shadow-lg w-64 bg-white h-screen">
+      <h1 className="font-bold text-2xl mb-4">You</h1>
+      <ul className="space-y-2">
+        <li className="text-gray-700 hover:text-black hover:bg-gray-200 rounded-md p-2 text-lg">Shorts</li>
+        <li className="text-gray-700 hover:text-black hover:bg-gray-200 rounded-md p-2 text-lg">History</li>
+        <li className="text-gray-700 hover:text-black hover:bg-gray-200 rounded-md p-2 text-lg">Liked Videos</li>
+        <li className="text-gray-700 hover:text-black hover:bg-gray-200 rounded-md p-2 text-lg">Playlists</li>
+      </ul>
+
+      <h1 className="font-bold text-2xl mt-6 mb-4">Subscriptions</h1>
+      <ul className="space-y-2">
+        <li className="text-gray-700 hover:text-black hover:bg-gray-200 rounded-md p-2 text-lg">Music</li>
+        <li className="text-gray-700 hover:text-black hover:bg-gray-200 rounded-md p-2 text-lg">Sports</li>
+        <li className="text-gray-700 hover:text-black hover:bg-gray-200 rounded-md p-2 text-lg">Gaming</li>
+        <li className="text-gray-700 hover:text-black hover:bg-gray-200 rounded-md p-2 text-lg">Movies</li>
+      </ul>
+
+      <h1 className="font-bold text-2xl mt-6 mb-4">Explore</h1>
+      <ul className="space-y-2">
+        <li className="text-gray-700 hover:text-black hover:bg-gray-200 rounded-md p-2 text-lg">Trending</li>
+        <li className="text-gray-700 hover:text-black hover:bg-gray-200 rounded-md p-2 text-lg">Shopping</li>
+        <li className="text-gray-700 hover:text-black hover:bg-gray-200 rounded-md p-2 text-lg">Gaming</li>
+        <li className="text-gray-700 hover:text-black hover:bg-gray-200 rounded-md p-2 text-lg">Courses</li>
+      </ul>
+    </div>
   )
 }
 
